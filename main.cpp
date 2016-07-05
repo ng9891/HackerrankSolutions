57739a2ba1ed539a02384304ec8e730ea8838800
David <davidng9891@gmail.com>
2016-07-05 03:08:03 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+	for(int i=0; i < n; i++){
+		for(int j=0; j < n; j++){
+			if(j < n-1 - i)
+				cout<<" ";
+			else
+				cout<<"#";
+		}
+		cout<<endl;
+	}
+    
+    
+    return 0;
+}
