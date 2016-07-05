97fb6e3f2ef2685a7c994c21094f347e51709680
David <davidng9891@gmail.com>
2016-07-05 02:56:07 -0400

    Update and rename AngryProfessor1/main.cpp to AngryProfessor/main.cpp

--- AngryProfessor/main.cpp
+++ AngryProfessor/main.cpp
@@ -1,3 +1,4 @@
+07/05/2016
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -7,25 +8,23 @@ using namespace std;
 
 int main(){
     int t;
-	int count=0;
+    int count=0;
     cin >> t;
     for(int a0 = 0; a0 < t; a0++){
-		count = 0;
+	count = 0;
         int n;
         int k;
         cin >> n >> k;
         vector<int> a(n);
         for(int a_i = 0;a_i < n;a_i++){
            cin >> a[a_i];
-
-		   if (a[a_i] <= 0)
-			   count++; // count student that arrived early.
+           if (a[a_i] <= 0)
+           count++; // count student that arrived early.
         }
-        
-		if (count < k)
-			cout<<"YES"<<endl;
-		else
-			cout<<"NO"<<endl;
+	if (count < k)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
     }
     return 0;
 }
